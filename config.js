2b8a7ccc926e7b24086665863c584d7bebb08f39
jay214421343 <48416316+jay214421343@users.noreply.github.com>
2019-05-20 16:15:10 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-exports.TOKEN = process.env.TOKEN;
+exports.TOKEN = process.env.NTc3NTI5NTczOTgyNjAxMjE4.XOKvjg.8WNVEw_EdLCkWWKVrRbq_OOPDjM;
 
 exports.GOOGLE_API_KEY = process.env.GOOGLE_API_KEY;
 
